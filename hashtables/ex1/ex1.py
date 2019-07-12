cee846f84db1ebe0761aa94fe5feaf706211d352
VaderSteve76 <alverson.s@yahoo.com>
2019-07-12 12:36:54 -0400

    Refactor ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,32 +7,20 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    if length <= 1:
-        return None
-
     ht = HashTable(16)
 
-    for i in range(length):  # check for element in hashtable
-        key_exist = hash_table_retrieve(ht, weights[i])
-        # is same elem. and each  = to elem + elem
-        if (key_exist == 0 or key_exist) and
-        (limit - weights[i] == weights[i]):
-            return (i, key_exist)
-        hash_table_insert(ht, weights[i], i)
+    for k, v in enumerate(weights):
+        hash_table_insert(ht, v, k)
 
-    keys = []  # placeholder
-
-    for i in range(length):   # looking for key, adding limit
-        key_value = hash_table_retrieve(ht, weights[i])
-        if key_value:
-            keys.append(weights[i])
-            keys.append(limit - weights[i])
-            break
-
-    indexes = [weights.index(keys[0]), weights.index(keys[1])]
-    indexes.sort(reverse=True)
-
-    return (indexes[0], indexes[1])
+    for k, v in enumerate(weights):
+        difference = limit - v
+        if hash_table_retrieve(ht, difference) is not None:
+            index = hash_table_retrieve(ht, difference)
+            if index >= k:
+                return [index, k]
+            else:
+                return [k, index]
+    return None
 
 
 def print_answer(answer):
